2ee97c41eed7bfadd9f8455db4bc85b9d6822615
Gustavo <esustenta@gmail.com>
2023-10-05 20:15:24 -0600

    Optimización de log de archivo
    ibid

--- LIBRERIA/activa_rotulos.py
+++ LIBRERIA/activa_rotulos.py
@@ -4,6 +4,8 @@ import arcpy
 import importlib
 import sys
 
+mxd = arcpy.env.mxd
+
 # Agrega la ruta del paquete al path de Python
 ruta_libreria = "Q:/09 SISTEMAS INFORMATICOS/GIS_PYTON/SOPORTE_GIS"
 sys.path.append(ruta_libreria)
@@ -14,26 +16,33 @@ log.log(u"Librería 'activa_rotulos' se ha cargado con éxito")
 
 def activar_rotulos(capa, campo_rotulo):
     log.log(u"Iniciando 'activar_rotulos'...")
-    capa = arcpy.mapping.Layer(capa)
-    # Habilita los rótulos para la capa
-    capa.showLabels = True
 
-    # Configura la expresión de rótulo
-    labelClass = capa.labelClasses[0]
-    labelClass.expression = "[{}]".format(campo_rotulo)
+    try:
+        capa = arcpy.mapping.Layer(capa)
+        # Habilita los rótulos para la capa
+        capa.showLabels = True
+        # Configura la expresión de rótulo
+        labelClass = capa.labelClasses[0]
+        labelClass.expression = "[{}]".format(campo_rotulo)
+        log.log(u"Rótulos activados satisfactoriamente'...")
+    except:
+        log.log(u">> ERROR, los rótulos no se han activado'...")
+
     log.log(u"'activar_rotulos' terminado...")
 
 
 def desactivar_rotulos(capa):
     log.log(u"Iniciando 'desactiva_rotulos'...")
-    # Obtiene el mapa actual
-    mxd = arcpy.mapping.MapDocument("CURRENT")
 
-    # Obtiene la capa activa del mapa
-    capa = arcpy.mapping.ListLayers(mxd, capa)[0]  # Ajusta el nombre de la capa según tu capa específica
+    try:
+        # Obtiene la capa activa del mapa
+        capa = arcpy.mapping.ListLayers(mxd, capa)[0]  # Ajusta el nombre de la capa según tu capa específica
+        # Habilita los rótulos para la capa
+        capa.showLabels = False
+        log.log(u"Rótulos desactivados satisfactoriamente'...")
+    except:
+        log.log(u">> ERROR, los rótulos no se han desactivado'...")
 
-    # Habilita los rótulos para la capa
-    capa.showLabels = False
     log.log(u"'desactivar_rotulos' terminado...")
 
     # Refresca la vista de datos para aplicar los cambios de rótulos
