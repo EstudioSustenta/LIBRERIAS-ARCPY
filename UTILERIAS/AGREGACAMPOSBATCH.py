354ee981e7f229f8b1ffb320efaa37fbbe615612
Gustavo <esustenta@gmail.com>
2024-01-03 18:50:02 -0600

    Código completamente funcional
    Se han arreglado todos los bugs.

--- UTILERIAS/AGREGACAMPOSBATCH.py
+++ UTILERIAS/AGREGACAMPOSBATCH.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 
-
 import arcpy
 import os
 import json
 from itertools import islice
 import codecs
+import traceback
+import time
 
-global archlog
 global campo
-archlog = u"Y:/GIS/MEXICO/VARIOS/INEGI/CENSALES/SCINCE 2020/00 log resultados.txt"
 
-def escribearch(cadena,modo):
-    with codecs.open(archlog,modo, encoding = 'utf-8') as archivo_log:
+
+def escribearch(cadena, modo, archlog):
+    with codecs.open(archlog, modo, encoding='utf-8') as archivo_log:
+        timestamp = time.time()     # Obtiene la fecha y hora actual en segundos desde la época (timestamp)
+        estructura_tiempo_local = time.localtime(timestamp)     # Convierte el timestamp a una estructura de tiempo local
+        fecha_hora_actual = time.strftime("%Y-%m-%d %H:%M:%S", estructura_tiempo_local)     # Formatea la estructura de tiempo local como una cadena legible
+
+        cadena = u"\n{}\t{}".format(cadena,fecha_hora_actual)
         archivo_log.write(cadena)
-        archivo_log.close
+        archivo_log.close()  # Corregir llamada a close()
+        print(cadena)
+
+
 
 def archjson(campo):
     # Ruta al archivo de texto
@@ -37,24 +45,20 @@ def archjson(campo):
     # ---finaliza rutina para asignar una descripción al campo
 
 
-def creacampo(archivo, camp, tipo, precision, longit):
-    # print ("'creacampo()' iniciando...")
-    escribearch("'creacampo' iniciando..",'a')
+def creacampo(archivo, camp, tipo, precision, longit, archlog):
+    escribearch(u"'creacampo' iniciando..",'a',archlog)
 
     if os.path.exists(archivo):
-        # print (archivo)
-        escribearch("El archivo \n'{}' \nexiste\n".format(archivo),'a')
+        escribearch(u"El archivo \n'{}' \nexiste\n".format(archivo),'a',archlog)
         
 
     camposs = [campo.name for campo in arcpy.ListFields(archivo)]
     
     if camp in camposs:       # Verificar si el campo existe
-        # print("El campo '{}' existe en el shapefile.".format(camp))
-        escribearch("El campo '{}' ya existe, no se creara".format(camp),'a')
+        escribearch(u"El campo '{}' ya existe, no se creará".format(camp),'a',archlog)
         # None
     else:
-        # print("El campo '{}' no existe en el shapefile, creando el campo.".format(camp))
-        escribearch("El campo '{}' no existe, no se creara".format(camp),'a')
+        escribearch(u"El campo '{}' no existe, se creará".format(camp),'a',archlog)
         arcpy.AddField_management(in_table=archivo,
                                 field_name=camp,
                                 field_type=tipo,
@@ -65,103 +69,95 @@ def creacampo(archivo, camp, tipo, precision, longit):
                                 field_is_nullable="NULLABLE",
                                 field_is_required="NON_REQUIRED",
                                 field_domain="")
-        # print("El campo {} ha sido creado en el shapefile.".format(camp))
-        
-
-    # print ("'creacampo()' finalizado.")
 
 
 def calcula():
 
+    archlog = u"Y:/GIS/MEXICO/VARIOS/INEGI/CENSALES/SCINCE 2020/00 log resultados.txt"
+
     arcpy.env.overwriteOutput = True
-    estados = [ # "Aguascalientes",
-                # "Baja California",
-                # "Baja California Sur",
-                # "Campeche",
-                # "Coahuila",
-                "Colima",
-                # "Chiapas",
-                # "Chihuahua",
-                # "Ciudad de México",
-                # "Durango",
-                # "Guanajuato",
-                # "Guerrero",
-                # "Hidalgo",
-                # "Jalisco",
-                # "México",
-                # "Michoacán de Ocampo",
-                # "Morelos",
-                # "Nayarit",
-                # "Nuevo León",
-                # "Oaxaca",
-                # "Puebla",
-                # "Querétaro",
-                # "Quintana Roo",
-                # "San Luis Potosí",
-                # "Sinaloa",
-                # "Sonora",
-                # "Tabasco",
-                # "Tamaulipas",
-                # "Tlaxcala",
-                # "Veracruz de Ignacio de la Llave",
-                # "Yucatán",
-                # "Zacatecas"
+    estados = [ # u"Aguascalientes",
+                # u"Baja California",
+                # u"Baja California Sur",
+                # u"Campeche",
+                # u"Coahuila",
+                # u"Colima",
+                # u"Chiapas",
+                # u"Chihuahua",
+                # u"Ciudad de México",
+                # u"Durango",
+                # u"Guanajuato",
+                # u"Guerrero",
+                # u"Hidalgo",
+                # u"Jalisco",
+                # u"México",
+                # u"Michoacán de Ocampo",
+                # u"Morelos",
+                # u"Nayarit",
+                # u"Nuevo León",
+                # u"Oaxaca",
+                # u"Puebla",
+                # u"Querétaro",
+                # u"Quintana Roo",
+                # u"San Luis Potosí",
+                # u"Sinaloa",
+                # u"Sonora",
+                # u"Tabasco",
+                # u"Tamaulipas",
+                # u"Tlaxcala",
+                # u"Veracruz de Ignacio de la Llave",
+                u"Yucatán",
+                # u"Zacatecas"
                 ]
 
     
-    escribearch("REPORTE DE INCIDENCIAS DE PROCESO 'AGREGARCAMPOSBATCH.py'\nGUSTAVO MARTINEZ VELASCO\n\n",'w')
+    escribearch(u"REPORTE DE INCIDENCIAS DE PROCESO 'AGREGARCAMPOSBATCH.py'\nGUSTAVO MARTINEZ VELASCO\n\n",'w',archlog)
 
     try:
-        escribearch("inicio de escritura",'a')
+        escribearch(u"inicio de escritura\n\n",'a',archlog)
         
         for estado in estados:
 
-            global archivo
             archtr = "manzana_localidad"
+            print (estado)
             archivo = u"Y:/GIS/MEXICO/VARIOS/INEGI/CENSALES/SCINCE 2020/{}/cartografia/{}.shp".format(estado,archtr)
 
-            print (estado)
-            print (u"\nEjecutando proceso, espere...\n\n")
-
-            escribearch(" INICIANDO PROCESO PARA...\n'{}\n'".format(estado),'a')
+            escribearch(u"'{}' \nIniciando proceso...".format(estado.upper()),'a',archlog)
 
             if os.path.exists(archivo):
 
-                # campo_a = ""
-                # campo36a = "POB36_a"
-
-                def area():
-                    print (u"Calculando campo de área")
+                def area(archivo):
+                    escribearch(u"\nProceso de área iniciando...",'a',archlog)
                     camp, tipo, precision, long = "area_has", "FLOAT", 9, ""
-                    escribearch("\n\t'{}' agregando...".format(camp),'a')
-                    creacampo(archivo, camp, tipo, precision, long)
-
-                    escribearch("\n\t'{}' agregado".format(camp),'a')
+                    
+                    creacampo(archivo, camp, tipo, precision, long, archlog)
 
                     expresion = "!shape.area@hectares!"
+                    escribearch(u"\tProceso de actualización de campos iniciando...",'a',archlog)
                     arcpy.CalculateField_management(archivo, camp, expresion, "PYTHON")
-                    escribearch("\n\t expresion '{}' agregada al archivo".format(expresion),'a')
-                    print (u"Finalizado proceso para campo de área")
+                    escribearch(u"\texpresión '{}' agregada al archivo".format(expresion),'a',archlog)
+                    escribearch(u"Proceso de área terminado",'a',archlog)
 
-                def densidad():
-
-                    print (u"Calculando campo de densidad")
+                def densidad(archivo):
+                    escribearch(u"\nProceso densidad iniciando...",'a',archlog)
 
                     camp, tipo, precision, long = "dens_ha", "LONG", 6, ""
-                    creacampo(archivo, camp, tipo, precision, long)
+                    creacampo(archivo, camp, tipo, precision, long, archlog)
 
                     expresion = "[POB1] / [area_has]"   # expresión en vb
 
+                    escribearch(u"\tProceso de actualización de campos iniciando...",'a',archlog)
+
                     arcpy.CalculateField_management(archivo, camp, expresion, expression_type="VB", code_block="")
-                    escribearch("\n\t expresion '{}' agregada al archivo".format(expresion),'a')
-                    print (u"Finalizado proceso para campo de densidad")
+                    escribearch(u"\t expresión '{}' agregada al archivo".format(expresion),'a',archlog)
+                    escribearch(u"Proceso densidad terminado",'a',archlog)
 
-                def pob36_a():
+                def pob36_a(archivo):
 
-                    print (u"Calculando campo de POB36_a")
+                    escribearch(u"\nProceso de 'POB36_a' iniciando...",'a',archlog)
 
                     camp, tipo, precision, long = "POB36_a", "SHORT", 6, ""
-                    creacampo(archivo, camp, tipo, precision, long)
+                    creacampo(archivo, camp, tipo, precision, long, archlog)
 
                     campos = ["POB1",
                                 "POB2",
@@ -184,6 +180,8 @@ def calcula():
                                 "POB41",
                                 "POB36_a"]
 
+                    escribearch(u"\tActualizando campos para 'POB36_a'",'a',archlog)
+
                     with arcpy.da.UpdateCursor(archivo, campos) as manzanas:
                         for num_manzana, manzana in enumerate(manzanas, start=0):
                         # for num_manzana, manzana in enumerate(islice(manzanas, 20), start=0): # linea limitante, util para pruebas de código
@@ -197,19 +195,21 @@ def calcula():
                                 manzana[-1] = -6
                                 manzanas.updateRow(manzana)
 
-                    print (u"Finalizado proceso para campo de POB36_a")
+                    escribearch(u"\tcampos para 'POB36_a' actualizados",'a',archlog)
 
-                def grupo_dom():
+                    escribearch(u"Proceso de 'POB36_a' terminado",'a',archlog)
 
-                    print (u"Calculando campo de grupo dominante")
+                def grupo_dom(archivo):
+
+                    escribearch(u"\nProceso para grupo dominante iniciando...",'a',archlog)
 
                     camp, tipo, precision, long = "ordinal", "TEXT", "", "150"
-                    creacampo(archivo, camp, tipo, precision, long)
+                    creacampo(archivo, camp, tipo, precision, long, archlog)
 
                     camp, tipo, precision, long = "pobdom", "TEXT", "", "150"
-                    creacampo(archivo, camp, tipo, precision, long)
+                    creacampo(archivo, camp, tipo, precision, long, archlog)
 
-                    escribearch("\n\tIniciando proceso para grupo dominante",'a')
+                    escribearch(u"\tIniciando proceso de actualización de campos...",'a',archlog)
                     campos = ["POB2",
                                 "POB4",
                                 "POB5",
@@ -262,45 +262,46 @@ def calcula():
                                 valmaxim = nuevalista
                                 valmaxim = ' '.join(valmaxim)
                                 domtext = (u"{} personas en {}".format(int(valmax), domtext))
-                                domtext = domtext.replace(u"Población de ", "")
+                                domtext = domtext.replace(u"Poblacion de ", "")
 
                             manzana[-2] = valmaxim
                             manzana[-1] = domtext
                             manzanas.updateRow(manzana)
+                    
+                    escribearch(u"\tProceso de actualización de campos terminado",'a',archlog)
+                    escribearch(u"Proceso para grupo dominante terminado",'a',archlog)
 
-
-                    print (u"Finalizado proceso para campo de grupo dominante")
-
-
-                def eliminar_campo_si_existe(nombre_campo):
+                def eliminar_campo_si_existe(nombre_campo,archivo):
+                    escribearch(u"Proceso para eliminar campos iniciando...",'a',archlog)
                     # Comprobar si el campo existe en la capa
                     campos = [campo.name for campo in arcpy.ListFields(archivo)]
                     
                     if nombre_campo in campos:
                         # Eliminar el campo
                         arcpy.DeleteField_management(archivo, nombre_campo)
-                        print("Campo '{}' eliminado exitosamente.".format(nombre_campo))
+                        escribearch(u"Campo '{}' eliminado exitosamente.".format(nombre_campo),'a',archlog)
                     else:
-                        print("El campo '{}' no existe en el shapefile.".format(nombre_campo))
+                        escribearch(u"El campo '{}' no existe en el shapefile.".format(nombre_campo),'a',archlog)
+                    
+                    escribearch(u"Proceso para eliminar campos terminado...",'a',archlog)
 
                 # Ruta del archivo shapefile
 
-                area()
-                densidad()
-                pob36_a()
-                grupo_dom()
-                # eliminar_campo_si_existe("densid_ha")
+                area(archivo)
+                densidad(archivo)
+                pob36_a(archivo)
+                grupo_dom(archivo)
+                # eliminar_campo_si_existe("densid_ha",archivo)
 
-                print (u"\n'{}' TERMINADO\n".format(estado))
-                escribearch("\n{} TERMINADO\n".format(estado),'a')
+                escribearch(u"\n{} TERMINADO\n".format(estado.upper()),'a',archlog)
 
             else:
-                print(u"El archivo \n{} \nno existe\n\n".format(estado))
-                escribearch("\n>ERROR.\t'{}' no existe\n".format(archivo),'a')
+                escribearch(u"\n>ERROR.\t'{}' no existe\n".format(archivo),'a',archlog)
 
     except Exception as err:
-        escribearch(">> ERROR DE PROCESO.\n   {}\n".format(err),'a')
-        print (err)
+        escribearch(u">> ERROR DE PROCESO.\n   {}\n".format(err),'a',archlog)
+        traceback.print_exc()
+
 
 # Llama a la función para ejecutar el código
 calcula()
