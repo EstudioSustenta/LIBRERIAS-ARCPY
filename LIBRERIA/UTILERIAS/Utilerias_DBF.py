5ea208a56d630803c60111dc1dd420208e4aa362
Gustavo <esustenta@gmail.com>
2024-04-20 19:04:27 -0600

    utilerias dbf semifuncional

--- LIBRERIA/UTILERIAS/Utilerias_DBF.py
+++ LIBRERIA/UTILERIAS/Utilerias_DBF.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from dbfread import DBF
+import traceback
 
-def recuperacamposdbf(archivo_dbf, campos, FIDS):
+def recuperacamposdbf(archivo_dbf, campos, FIDS, codificacion='utf-8'):
     diccionario = []
     for FID in FIDS:
         try:
@@ -11,17 +12,17 @@ def recuperacamposdbf(archivo_dbf, campos, FIDS):
 
             valores_campos = {}
 
-            codificaciones = {
-                "conjunto_de_datos/denue_wgs84z13.dbf": "latin-1",
-                "USO DE SUELO INEGI SERIE IVCopy.dbf": "ISO-8859-1",
-            }
+            # codificaciones = {
+            #     "conjunto_de_datos/denue_wgs84z13.dbf": "latin-1",
+            #     "USO DE SUELO INEGI SERIE IVCopy.dbf": "ISO-8859-1",
+            # }
 
-            for codigo in codificaciones:
-                if codigo in archivo_dbf:
-                    codificacion = codificaciones[codigo]
-                    break
-                else:
-                    codificacion = 'utf-8'
+            # for codigo in codificaciones:
+            #     if codigo in archivo_dbf:
+            #         codificacion = codificaciones[codigo]
+            #         break
+            #     else:
+            #         codificacion = 'utf-8'
 
             with DBF(archivo_dbf, encoding=codificacion) as dbf:
                 for i, registro in enumerate(dbf):
@@ -57,7 +58,7 @@ def cant_de_registros(archivo):
     
     from dbfread import DBF
 
-def obtener_distancias(archivo_dbf,codificacion):
+def obtener_distancias(archivo_dbf,campo,codificacion):
     
     """
     Función para obtener un diccionario con el número de registro como clave
@@ -68,25 +69,47 @@ def obtener_distancias(archivo_dbf,codificacion):
     - archivo_dbf: Ruta del archivo DBF.
 
     Returns:
-    - Un diccionario con el número de registro como clave y el valor del campo 'distancia' como valor.
+    - Un diccionario con el número de registro como clave y el valor del campo como valor.
     """
-    distancias = {}
+    valores = {}
     try:
         with DBF(archivo_dbf, encoding=codificacion) as dbf:
             for i, registro in enumerate(dbf):
-                distancias[i] = registro.get('distancia', 'Valor no disponible')
+                fid = registro.get('IN_FID', 'Valor no disponible')
+                valores[fid] = registro.get(campo, 'Valor no disponible')
+                # valores[i] = registro
     except Exception as e:
         print("Error al leer el archivo DBF:", e)
-    return distancias
+    return valores
 
 
 
 if __name__=="__main__":
 
-    # Ruta al archivo DBF
-    archivo_dbf = "Y:/GIS/MEXICO/VARIOS/INEGI/Mapa Digital 6/WGS84/USO DE SUELO INEGI SERIE IVCopy.dbf"
-    # Campos que deseas recuperar
-    campos = ['CVE_UNION', 'ECOS_VEGE', 'DESVEG', 'FORMACION', 'HECTARES', 'CVE_FAO', 'VEG_FORES', 'INFYS_0409', 'Sup_ha', 'Sup_hasa']
+    def recuperac():
 
-    resultados = recuperacamposdbf(archivo_dbf)
-    print(resultados)
+        # Ruta al archivo DBF
+        archivo_dbf = "Y:/GIS/MEXICO/VARIOS/INEGI/DENUE/2023/Aguascalientes/conjunto_de_datos/denue_wgs84z13.dbf"
+        # Campos que deseas recuperar
+        campos = ['nom_estab', 'raz_social', 'nombre_act', 'nom_vial']
+        fids=[156,842,846]
+        codificacion="latin-1"
+
+        resultados = recuperacamposdbf(archivo_dbf, campos,fids,codificacion)
+        if type(resultados) is list:
+            for resultado in resultados:
+                print(resultado)
+        else:
+            print(resultados)
+    
+    def obtenerd():
+
+        archivo_dbf = "Y:/02 CLIENTES (EEX-CLI)/00 prueba impresion/02/temp/denue_near_tmp.dbf"
+        codificacion="latin-1"
+        campo="NEAR_DIST"
+        resultados=obtener_distancias(archivo_dbf,campo, codificacion)
+        for resultado in resultados:
+
+            print(resultado, ":",resultados[resultado])
+
+    obtenerd()
